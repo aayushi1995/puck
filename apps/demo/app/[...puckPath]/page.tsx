45fb8c87c9f0febba0ad17cb05ad7c8348063420
aayushi Kambariya <aayushi.kambriya5@gmail.com>
2024-08-07 00:21:36 +0530

    fix: Remove unneccessary code

--- apps/demo/app/[...puckPath]/page.tsx
+++ apps/demo/app/[...puckPath]/page.tsx
@@ -31,5 +31,5 @@ export default async function Page({
 }) {
   const { isEdit, path } = resolvePuckPath(params.puckPath);
 
-  return <div style={{border: '1px solid red'}}><Client isEdit={isEdit} path={path} /></div>;
+  return <Client isEdit={isEdit} path={path} />;
 }
